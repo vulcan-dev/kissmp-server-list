2166754c6bb4a7f7ff1ca04a7855168df0439026
Daniel W <danielw@pop-os.localdomain>
2021-06-05 02:31:40 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kissmp-server-list
-Simple KissMP Serverlist
+This is just a basic server list for KissMP (BeamNG Multiplayer Mod)
+
+As of right now there's no sorting but I plan on adding this at some point
+- [ ] Add sorting
+- [ ] Connect to server (I think it's possible)
+- [ ] Tweak a bit of the CSS to make it look better
+- [ ] Add a searchbar
---
+++ js/index.js
@@ -0,0 +1,74 @@
+const serverAddress = 'http://kissmp.online:3692/0.4.4';
+var serverCount = 0;
+var lastRow = -1;
+
+function hideRow(row) {
+    $('#' + lastRow).toggle();
+    $('#' + row).toggle();
+    lastRow = row;
+}
+
+function addToTable(serverName, currentPlayers, description, ip, map) {
+    $('.content-table > tbody').append(`
+    <tr onclick="hideRow('hidden_row${serverCount}')" class="hover">
+        <td>${serverName}</td>
+        <td>${currentPlayers}</td>
+        <td>${map}</td>
+    </tr>
+    
+    <tr id="hidden_row${serverCount}" class="hidden_row">
+        <td colspan="4">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>IP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>${description}</td>
+                        <td>
+                            <div class="tooltip"><a href="#">${ip}
+                                <span class="tooltiptext">Connect to server</span>
+                            </a></div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    `)
+}
+
+function retreiveData(data) {
+    for (const key in data) {
+        serverCount++;
+        addToTable(data[key].name, data[key].player_count, data[key].description, key, data[key].map)
+    }
+}
+
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open('GET', serverAddress)
+xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*')
+xmlHttp.setRequestHeader('Access-Control-Allow-Methods', 'GET')
+xmlHttp.setRequestHeader('Access-Control-Allow-Headers', 'accept, content-type')
+
+xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        retreiveData(JSON.parse(this.response));
+    }
+}
+
+/**
+ * name
+ * player_count
+ * max_players
+ * description
+ * map
+ * port
+ * version
+ */
+
+xmlHttp.open('GET', serverAddress);
+xmlHttp.send();
